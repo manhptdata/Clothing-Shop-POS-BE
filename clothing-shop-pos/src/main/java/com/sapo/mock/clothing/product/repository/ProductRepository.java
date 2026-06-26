961d2b5c859a4d6da2e6189aa5f9de32a960bda1
manhpt <phamtienmanh27122003@gmail.com>
2026-06-26 11:18:52 +0700

    update verify before delete category

--- clothing-shop-pos/src/main/java/com/sapo/mock/clothing/product/repository/ProductRepository.java
+++ clothing-shop-pos/src/main/java/com/sapo/mock/clothing/product/repository/ProductRepository.java
@@ -2,8 +2,12 @@ package com.sapo.mock.clothing.product.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.sapo.mock.clothing.entity.Category;
 import com.sapo.mock.clothing.entity.Product;
 
 @Repository
@@ -11,4 +15,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer>, JpaS
 	boolean existsByCategory_Id(Integer categoryId);
 
 	boolean existsByName(String name);
+
+	@Modifying
+	@Query("UPDATE Product p SET p.category = :newCategory WHERE p.category.id = :oldCategoryId")
+	void updateCategoryForProducts(@Param("oldCategoryId") Integer oldCategoryId, @Param("newCategory") Category newCategory);
 }
